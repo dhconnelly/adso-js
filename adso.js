e29a94e22175c1ec69ac8381a7847ba8c7ec5253
Daniel Connelly <dhconnelly@gmail.com>
2019-07-20 10:42:32 -0400

    implement interpreter

--- adso.js
+++ adso.js
@@ -1,5 +1,217 @@
 'use strict';
 
+const util = require('util');
+
+function assert(val, msg='') {
+  if (!val) {
+    throw new Error(`assertion error: ${val} (${msg})`);
+  }
+}
+
+class InterpreterError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'InterpreterError';
+  }
+}
+
+class Interpreter {
+  constructor() {
+    this.scope = { bindings: {
+      print: {
+        type: 'BuiltInFn',
+        param_type: 'int',
+        value: function(obj) {
+          console.log(obj.value);
+        },
+      }
+    } };
+    this.stack = [];
+  }
+
+  pushScope() {
+    let scope = { bindings: {} };
+    if (this.scope) {
+      scope.up = this.scope;
+    }
+    this.scope = scope;
+  }
+
+  popScope() {
+    this.scope = this.scope.up;
+  }
+
+  visitArithExpr(expr) {
+    let leftValue;
+    switch (expr.left.type) {
+      case 'number': {
+        leftValue = expr.left.value;
+        break;
+      }
+      case 'ident': {
+        let binding = this.lookup(expr.left.value);
+        if (binding.type !== 'int') {
+          throw new InterpreterError(
+            `bad binding ${util.inspect(binding)} ` +
+            `in expr ${util.inspect(expr)}`);
+        }
+        leftValue = binding.value;
+        break;
+      }
+      default:
+        throw new InterpreterError(
+          `bad left ${util.inspect(expr.left)} ` +
+          `in expr ${util.inspect(expr)}`);
+    }
+    this.visitExpr(expr.right);
+    let right = this.stack.pop();
+    assert(
+      right.type === 'int',
+      `bad right ${util.inspect(right)} ` +
+      `in expr ${util.inspect(expr)}`);
+    let rightValue = right.value;
+    switch (expr.op) {
+      case '<': return this.stack.push({
+        type: 'bool',
+        value: leftValue < rightValue,
+      });
+      case '*': return this.stack.push({ 
+        type: 'int', 
+        value: leftValue * rightValue 
+      });
+      case '-': return this.stack.push({ 
+        type: 'int', 
+        value: leftValue - rightValue 
+      });
+    }
+  }
+
+  visitExpr(expr) {
+    switch (expr.type) {
+      case 'number': {
+        return this.stack.push({
+          type: 'int',
+          value: expr.value,
+        });
+      }
+      case 'ident': {
+        let binding = this.lookup(expr.value);
+        return this.stack.push(binding);
+      }
+      case 'FnCall': return this.visitFnCall(expr);
+      case 'ArithExpr': return this.visitArithExpr(expr);
+    }
+    assert(false);
+  }
+
+  visitFnDef(fnDef) {
+    this.scope.bindings[fnDef.name] = fnDef;
+  }
+
+  checkType(fnDef, argValue) {
+    if (!fnDef.param_type && argValue) {
+      throw new InterpreterError(
+        `no arg expected for ${util.inspect(fnDef)}, got ${argValue.type}`);
+    } else if (fnDef.param_type && !argValue) {
+      throw new InterpreterError(`no fn arg given for ${util.inspect(fnDef)}`);
+    } else if (fnDef.param_type !== argValue.type ) {
+      let expected = fnDef.param_type;
+      let actual = argValue.type;
+      throw new InterpreterError(
+        `type mismatch for fn ${util.inspect(fnDef)}, ` +
+        `arg ${util.inspect(argValue)}`);
+    }
+  }
+
+  visitReturnSt(returnSt) {
+    this.visitExpr(returnSt.value);
+    throw { type: 'ReturnValue', value: this.stack.pop() };
+  }
+
+  visitIfSt(ifSt) {
+    this.visitExpr(ifSt.condition);
+    let condition = this.stack.pop();
+    if (condition.type !== 'bool') {
+      throw new InterpreterError(
+        `if condition not bool: ${util.inspect(condition)}`);
+    }
+    if (condition.value) {
+      ifSt.body.forEach((node) => this.visitSt(node));
+    }
+  }
+
+  visitSt(st) {
+    switch (st.type) {
+      case 'IfSt': return this.visitIfSt(st);
+      case 'ReturnSt': return this.visitReturnSt(st);
+      case 'FnCall': return this.visitFnCall(st);
+    }
+  }
+
+  lookup(name, scope=this.scope) {
+    let binding = scope.bindings[name];
+    if (binding) {
+      return binding;
+    } else if (!binding && scope.up) {
+      return this.lookup(name, scope.up);
+    } else {
+      throw new InterpreterError(`${name} is unbound`);
+    }
+  }
+
+  visitFnCall(fnCall) {
+    let fnDef = this.lookup(fnCall.name);
+    if (!['BuiltInFn', 'FnDef'].includes(fnDef.type)) {
+      throw new InterpreterError(`not a function: ${fnCall.name}`);
+    }
+    let argValue;
+    if (fnCall.arg) {
+      this.visitExpr(fnCall.arg);
+      argValue = this.stack.pop();
+    }
+    if (fnCall.arg || fnDef.param_type ) {
+      this.checkType(fnDef, argValue);
+    }
+    this.pushScope();
+    if (fnCall.arg) {
+      this.scope.bindings[fnDef.param_name] = argValue;
+    }
+    if (fnDef.type === 'FnDef') {
+      try {
+        fnDef.body.forEach((st) => this.visitSt(st));
+      } catch (err) {
+        if (!err.type || err.type !== 'ReturnValue') {
+          throw err;
+        } else {
+          this.stack.push(err.value);
+        }
+      }
+    } else {
+      assert(fnDef.type === 'BuiltInFn');
+      fnDef.value(argValue);
+    }
+    this.popScope();
+
+    // get the return value and re-push it to be explicit
+    let retVal = this.stack.pop();
+    this.stack.push(retVal);
+  }
+}
+
+function visit(ast, visitor) {
+  ast.body.forEach((node) => {
+    switch (node.type) {
+      case 'FnDef': return visitor.visitFnDef(node);
+      case 'FnCall': return visitor.visitFnCall(node);
+    }
+  });
+}
+
+function interpret(ast) {
+  ast.body.push({ type: 'FnCall', name: 'main', arg: null });
+  visit(ast, new Interpreter());
+}
+
 // syntactical structure
 // ArithExpr := Expr ('*' | '-') Expr ;
 // Expr := number | ident | FnCall | ArithExpr ;
@@ -88,7 +300,7 @@ function parse(lexer) {
     eat('FnCall', { value: ')' });
     return {
       type: 'FnCall',
-      fn_name: name,
+      name: name,
       arg: arg,
     };
   }
@@ -293,8 +505,6 @@ function fail(message) {
   process.exit(1);
 }
 
-const util = require('util');
-
 function main() {
   const fs = require('fs');
   const args = process.argv.slice(2);
@@ -310,7 +520,7 @@ function main() {
   }
   let lexer = new Lexer(text);
   let ast = parse(lexer);
-  console.log(util.inspect(ast, { depth: null }));
+  interpret(ast);
 }
 
 main();
